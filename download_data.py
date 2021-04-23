f9c26dc7bd78160a28ba02273e21fdd1aa38baa0
TechyNilesh <49296182+TechyNilesh@users.noreply.github.com>
2021-04-23 23:00:22 +0530

    Add files via upload

---
+++ download_data.py
@@ -0,0 +1,5 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='18Q8K9r8hA8D8P0S0dEg_SJOquz5sqEw2',
+                                    dest_path='./flower_data.zip',
+                                    unzip=True)
